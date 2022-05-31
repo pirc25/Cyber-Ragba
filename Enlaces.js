f5585642870b5aa256876bdb90dddf9d66ec52c6
Richard Burneo <rchrdbrneo@gmail.com>
2022-05-31 17:55:07 -0500

    Update fuction with JS

---
+++ Enlaces.js
@@ -0,0 +1,143 @@
+
+export const enlaces = [{
+  id :1,
+  url: "https://app.iess.gob.ec/iess-gestion-desbloqueo-publico-web/" ,
+  nombre: "Generación Clave IESS",
+  institucion: "IESS",
+},
+{
+   id :2,
+  url: "https://pq.biess.fin.ec/pq-concesion-web/pages/concesion/roles.jsf" ,
+  nombre: "Préstamos Quirografarios Biess PQ",
+  institucion: "BIESS",
+},
+{
+  id: 3,
+  url: "https://ph.biess.fin.ec/ph-web/main.jsf" ,
+  nombre: "Présta Hipotecarios Biess PH",
+  institucion: "BIESS",
+},
+{
+  id: 4,
+  url: "https://www.iess.gob.ec/afiliado-web/pages/principal.jsf" ,
+  nombre: "Hisotria laboral IESS",
+  institucion: "IESS",
+},
+{
+  id: 5,
+  url: "https://app.iess.gob.ec/iess-gestion-cesantia-afiliado-web/app/index?comodin=C" ,
+  nombre: "Cesantía",
+  institucion: "IESS",
+},
+{
+  id:6,
+  url: "https://app.iess.gob.ec/iess-fondo-reserva-afiliado-web/app/index" ,
+  nombre: "Fondos de Reserva ",
+  institucion: "IESS",
+},
+
+{
+  id:7,
+  url: "https://www.iess.gob.ec/afiliado-web/pages/opcionesGenerales/eleccionCertificadoDeAfiliacion.jsf" ,
+  nombre: "Certificado de afiliación",
+  institucion: "IESS",
+},
+
+{
+  id:8,
+  url: "https://www.iess.gob.ec/empleador-web/pages/principal.jsf" ,
+  nombre: "Sistema de Empleadores",
+  institucion: "IESS",
+},
+
+{
+  id:9,
+  url: "https://www.iess.gob.ec/prjPensionesJubilacion-web/pages/principal.jsf" ,
+  nombre: "Jubilados",
+  institucion: "IESS",
+},
+
+{
+  id:10,
+  url: "https://srienlinea.sri.gob.ec/auth/realms/Internet/protocol/openid-connect/auth?client_id=app-sri-claves-angular&redirect_uri=https%3A%2F%2Fsrienlinea.sri.gob.ec%2Fsri-en-linea%2F%2Fcontribuyente%2Fperfil&state=4381952f-8d38-4b9b-9846-50c3970fbb44&nonce=cdea837a-3994-42fd-8bec-25c8bc5326d0&response_mode=fragment&response_type=code&scope=openid" ,
+  nombre: "SRI",
+  institucion: "SRI",
+},
+
+{
+  id:11,
+  url: "https://appsj.funcionjudicial.gob.ec/documentosExtraviados/publico/formulario.jsf" ,
+  nombre: "Documentos Extravíados",
+  institucion: "Judicial",
+},
+
+{
+  id:12,
+  url: "https://servicios.contraloria.gob.ec:4443/cge_arquitecturaonline_web/wFLogin.aspx?opc=1" ,
+  nombre: "Contraloría",
+  institucion: "Contraloría",
+},
+
+{
+  id:13,
+  url: "https://consultaweb.ant.gob.ec/SVT/paginas/portal/svf_solicitar_servicio.jsp?ps_param_tip_serv=CER" ,
+  nombre: "Certificados Ant (Certificado único vehicular)",
+  institucion: "ANT",
+},
+
+{
+  id:14,
+  url: "https://consultaweb.ant.gob.ec/SVT/paginas/portal/svf_solicitar_servicio.jsp?ps_param_tip_serv=LIC" ,
+  nombre: "Turnos ANT",
+  institucion: "ANT",
+},
+
+{
+  id:15,
+  url: "https://apps.registrocivil.gob.ec/portalCiudadano/login.jsf" ,
+  nombre: "Registro Civil",
+  institucion: "Registro Civil",
+},
+
+{
+  id:16,
+  url: "https://mail.loja.gob.ec:8443/;jsessionid=lkoi4edzbup21vgay6hg4i0u9?loginOp=relogin&client=socialfox&loginErrorCode=service.AUTH_REQUIRED" ,
+  nombre: "Zimbra Municipio Loja",
+  institucion: "Municipio Loja",
+},
+
+{
+  id:17,
+  url: "https://accounts.google.com/ServiceLogin/identifier?service=mail&passive=1209600&osid=1&continue=https%3A%2F%2Fmail.google.com%2Fmail%2Fu%2F0%2F&followup=https%3A%2F%2Fmail.google.com%2Fmail%2Fu%2F0%2F&emr=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin" ,
+  nombre: "Gmail",
+  institucion: "Google",
+},
+
+{
+  id:18,
+  url: "https://outlook.live.com/owa/" ,
+  nombre: "Hotmail",
+  institucion: "Hotmail",
+},
+
+{
+  id:19,
+  url: "https://www.rplojavirtual.gob.ec/login.xhtml" ,
+  nombre: "Registro de la propiedad",
+  institucion: "Registro de la propiedad",
+},
+
+{
+  id:20,
+  url: "http://www.c-movil.com.ec/Login.aspx" ,
+  nombre: "Recargas CMovil",
+  institucion: "CMovil",
+},
+
+{
+  id:21,
+  url: "https://fullcarga-titan.com.ec/" ,
+  nombre: "Recargas Fullcarga",
+  institucion: "fullcarga",
+},
+]
---
+++ app.js
@@ -0,0 +1,18 @@
+import {enlaces} from "./Enlaces.js";
+
+function getElements() {
+  const sitios = document.getElementById("sitios");
+};
+
+function mostrarEnlaces() {
+  let elementoA = '';
+  enlaces.forEach(enlace => { elementoA += `<a href= ${enlace.url} class='enlace'>${enlace.nombre}</a>`   
+  });
+  sitios.innerHTML = elementoA;
+  };
+
+function main() {
+ mostrarEnlaces(enlaces);
+}
+
+main ();

75b82a28c48f769361e4e7085666f694d778fff4
Richard Burneo <rchrdbrneo@gmail.com>
2024-05-09 17:47:02 -0500

    Funcionalidad Buscar

--- app.js
+++ app.js
@@ -1,18 +1,42 @@
-import {enlaces} from "./Enlaces.js";
+import { enlaces } from "./Enlaces.js";
 
 function getElements() {
   const sitios = document.getElementById("sitios");
-};
-
-function mostrarEnlaces() {
-  let elementoA = '';
-  enlaces.forEach(enlace => { elementoA += `<a href= ${enlace.url} class='enlace' target='_blank'>${enlace.nombre}</a>`   
-  });
-  sitios.innerHTML = elementoA;
-  };
-
-function main() {
- mostrarEnlaces(enlaces);
 }
 
-main ();
+function mostrarEnlaces() {
+  let elementoA = "";
+  enlaces.forEach((enlace) => {
+    elementoA += `<a href= ${enlace.url} class='enlace' target='_blank'>${enlace.nombre}</a>`;
+  });
+  sitios.innerHTML = elementoA;
+}
+
+function buscarPorNombre(enlaces, valorBusqueda) {
+  return enlaces.filter((enlace) =>
+    enlace.nombre.toLowerCase().includes(valorBusqueda.toLowerCase())
+  );
+}
+
+function mostrarResultados(enlaces) {
+  let elementoA = "";
+  enlaces.forEach((enlace) => {
+    elementoA += `<a href= ${enlace.url} class='enlace' target='_blank'>${enlace.nombre}</a>`;
+  });
+  sitios.innerHTML = elementoA;
+}
+
+function buscar() {
+  console.log(document.getElementById("input").value.toLowerCase());
+  const valorBusqueda = document.getElementById("input").value.toLowerCase();
+  const resultados = buscarPorNombre(enlaces, valorBusqueda);
+  mostrarResultados(resultados);
+}
+window.buscar = buscar;
+window.mostrarEnlaces = mostrarEnlaces;
+
+function main() {
+  mostrarEnlaces(enlaces);
+}
+
+main();

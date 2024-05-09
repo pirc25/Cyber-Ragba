c7017d026d56ae86296625d1a51e5326abde28b3
Richard Burneo <rchrdbrneo@gmail.com>
2024-05-09 06:57:24 -0500

    nuevos links

--- Enlaces.js
+++ Enlaces.js
@@ -1,199 +1,254 @@
+export const enlaces = [
+  {
+    id: 1,
+    url: "https://app.iess.gob.ec/iess-gestion-desbloqueo-publico-web/",
+    nombre: "Generación Clave IESS",
+    institucion: "IESS",
+  },
+  {
+    id: 2,
+    url: "https://pq.biess.fin.ec/",
+    nombre: "Préstamos Quirografarios Biess PQ",
+    institucion: "BIESS",
+  },
+  {
+    id: 3,
+    url: "https://ph.biess.fin.ec",
+    nombre: "Préstamos Hipotecarios Biess PH",
+    institucion: "BIESS",
+  },
+  {
+    id: 24,
+    url: "https://app.iess.gob.ec/iess-gestion-turnero-enlinea-web",
+    nombre: "Turno IESS",
+    institucion: "IESS",
+  },
+  {
+    id: 23,
+    url: " https://app.iess.gob.ec/iess-gestion-desbloqueo-clave-web/public/desbloquearClave.jsf",
+    nombre: "Desbloqueo claves",
+    institucion: "IESS",
+  },
 
-export const enlaces = [{
-  id :1,
-  url: "https://app.iess.gob.ec/iess-gestion-desbloqueo-publico-web/" ,
-  nombre: "Generación Clave IESS",
-  institucion: "IESS",
-},
-{
-   id :2,
-  url: "https://pq.biess.fin.ec/" ,
-  nombre: "Préstamos Quirografarios Biess PQ",
-  institucion: "BIESS",
-},
-{
-  id: 3,
-  url: "https://ph.biess.fin.ec" ,
-  nombre: "Préstamos Hipotecarios Biess PH",
-  institucion: "BIESS",
-},
-{
-  id: 24,
-  url: "https://app.iess.gob.ec/iess-gestion-turnero-enlinea-web" ,
-  nombre: "Turno IESS",
-  institucion: "IESS",
-},
-{                        
- id:23,
-  url: " https://app.iess.gob.ec/iess-gestion-desbloqueo-clave-web/public/desbloquearClave.jsf" ,
-  nombre: "Desbloqueo claves",
-  institucion: "IESS",
-},                  
-{
-  id: 4,
-  url: "https://www.iess.gob.ec/afiliado-web/pages/principal.jsf" ,
-  nombre: "Historia laboral IESS",
-  institucion: "IESS",
-},
-{
-  id: 5,
-  url: "https://app.iess.gob.ec/iess-gestion-cesantia-afiliado-web/app/index?comodin=C" ,
-  nombre: "Cesantía",
-  institucion: "IESS",
-},
-{
-  id:6,
-  url: "https://app.iess.gob.ec/iess-fondo-reserva-afiliado-web/app/index" ,
-  nombre: "Fondos de Reserva",
-  institucion: "IESS",
-},
+  {
+    id: 37,
+    url: "https://www.iess.gob.ec/seguridades-web/pages/public/login.jsf",
+    nombre: "Cambio de clave",
+    institucion: "IESS",
+  },
 
-{
-  id:7,
-  url: "https://www.iess.gob.ec/afiliado-web/pages/opcionesGenerales/seleccionCertificadoDeAfiliacion.jsf" ,
-  nombre: "Certificado de afiliación",
-  institucion: "IESS",
-},
+  {
+    id: 4,
+    url: "https://www.iess.gob.ec/afiliado-web/pages/principal.jsf",
+    nombre: "Historia laboral IESS",
+    institucion: "IESS",
+  },
+  {
+    id: 29,
+    url: "https://app.iess.gob.ec/gestion-afiliado-web/app/index",
+    nombre: "Resumen y detalles de aportes IESS",
+    institucion: "IESS",
+  },
+  {
+    id: 5,
+    url: "https://app.iess.gob.ec/iess-gestion-cesantia-afiliado-web/app/index?comodin=C",
+    nombre: "Cesantía",
+    institucion: "IESS",
+  },
+  {
+    id: 6,
+    url: "https://app.iess.gob.ec/iess-fondo-reserva-afiliado-web/app/index",
+    nombre: "Fondos de Reserva",
+    institucion: "IESS",
+  },
 
-{
-  id:8,
-  url: "https://www.iess.gob.ec/empleador-web/pages/principal.jsf" ,
-  nombre: "Sistema de Empleadores",
-  institucion: "IESS",
-},
+  {
+    id: 7,
+    url: "https://www.iess.gob.ec/afiliado-web/pages/opcionesGenerales/seleccionCertificadoDeAfiliacion.jsf",
+    nombre: "Certificado de afiliación",
+    institucion: "IESS",
+  },
 
-{
-  id:9,
-  url: "https://www.iess.gob.ec/pensionistas/" ,
-  nombre: "Jubilados o Pensionistas",
-  institucion: "IESS",
-},
+  {
+    id: 33,
+    url: "https://www.iess.gob.ec/empleador-web/pages/morapatronal/certificadoCumplimientoPublico.jsf",
+    nombre: "Certificado de obligaciones patronales",
+    institucion: "IESS",
+  },
 
-{
-  id:10,
-  url: "https://srienlinea.sri.gob.ec/sri-en-linea/inicio/NAT" ,
-  nombre: "SRI",
-  institucion: "SRI",
-},
+  {
+    id: 34,
+    url: "https://www.iess.gob.ec/empleador-web/pages/morapatronal/certificadoCumplimientoPublico.jsf",
+    nombre: "Certificado de obligaciones patronales",
+    institucion: "IESS",
+  },
 
-{
-  id:11,
-  url: "https://appsj.funcionjudicial.gob.ec/documentosExtraviados/publico/formulario.jsf" ,
-  nombre: "Documentos Extravíados",
-  institucion: "Judicial",
-},
+  {
+    id: 35,
+    url: "https://app.iess.gob.ec/iess-gestion-recaudacion-cartera-empleador-web/app/index",
+    nombre: "Acceder acuerdos de pagos parciales en línea",
+    institucion: "IESS",
+  },
 
-{
-  id:12,
-  url: "https://servicios.contraloria.gob.ec:4443/cge_arquitecturaonline_web/wFLogin.aspx?opc=1" ,
-  nombre: "Contraloría",
-  institucion: "Contraloría",
-},
+  {
+    id: 9,
+    url: "https://www.iess.gob.ec/pensionistas/",
+    nombre: "Jubilados o Pensionistas",
+    institucion: "IESS",
+  },
 
-{
-  id:13,
-  url: "https://consultaweb.ant.gob.ec/SVT/paginas/portal/svf_solicitar_servicio.jsp?ps_param_tip_serv=CER" ,
-  nombre: "Certificados Ant (Certificado único vehicular)",
-  institucion: "ANT",
-},
+  {
+    id: 31,
+    url: "https://www.iess.gob.ec/prjPensionesJubilacion-web/pages/principal.jsf",
+    nombre: "Rol Jubilados",
+    institucion: "IESS",
+  },
 
-{
-  id:14,
-  url: "https://consultaweb.ant.gob.ec/SVT/paginas/portal/svf_solicitar_servicio.jsp?ps_param_tip_serv=LIC" ,
-  nombre: "Turnos ANT",
-  institucion: "ANT",
-},
+  {
+    id: 32,
+    url: "https://app.iess.gob.ec/gestion-pension-jubilacion-afiliado-web/app/index",
+    nombre: "Certificado de pensionistas",
+    institucion: "IESS",
+  },
 
-{
-  id:15,
-  url: "https://apps.registrocivil.gob.ec/portalCiudadano/login.jsf" ,
-  nombre: "Registro Civil",
-  institucion: "Registro Civil",
-},
+  {
+    id: 10,
+    url: "https://srienlinea.sri.gob.ec/sri-en-linea/inicio/NAT",
+    nombre: "SRI",
+    institucion: "SRI",
+  },
 
-{
-  id:16,
-  url: "https://mail.loja.gob.ec:8443" ,
-  nombre: "Zimbra Municipio Loja",
-  institucion: "Municipio Loja",
-},
+  {
+    id: 30,
+    url: "https://app02.cne.gob.ec/sscne/Login.aspx",
+    nombre: "Tramites CNE",
+    institucion: "CNE",
+  },
 
-{
-  id:17,
-  url: "https://gmail.com" ,
-  nombre: "Gmail",
-  institucion: "Google",
-},
+  {
+    id: 11,
+    url: "https://appsj.funcionjudicial.gob.ec/documentosExtraviados/publico/formulario.jsf",
+    nombre: "Documentos Extravíados",
+    institucion: "Judicial",
+  },
 
-{
-  id:18,
-  url: "https://outlook.live.com/owa/" ,
-  nombre: "Hotmail",
-  institucion: "Hotmail",
-},
+  {
+    id: 12,
+    url: "https://servicios.contraloria.gob.ec:4443/cge_arquitecturaonline_web/wFLogin.aspx?opc=1",
+    nombre: "Contraloría",
+    institucion: "Contraloría",
+  },
 
-{
-  id:19,
-  url: "https://www.rplojavirtual.gob.ec/login.xhtml" ,
-  nombre: "Registro de la propiedad",
-  institucion: "Registro de la propiedad",
-},
+  {
+    id: 13,
+    url: "https://consultaweb.ant.gob.ec/SVT/paginas/portal/svf_solicitar_servicio.jsp?ps_param_tip_serv=CER",
+    nombre: "Certificados Ant (Certificado único vehicular)",
+    institucion: "ANT",
+  },
 
-{
-  id:20,
-  url: "http://www.c-movil.com.ec/Login.aspx" ,
-  nombre: "Recargas CMovil",
-  institucion: "CMovil",
-},
+  {
+    id: 14,
+    url: "https://consultaweb.ant.gob.ec/SVT/paginas/portal/svf_solicitar_servicio.jsp?ps_param_tip_serv=LIC",
+    nombre: "Turnos ANT",
+    institucion: "ANT",
+  },
 
-{
-  id:21,
-  url: "https://fullcarga-titan.com.ec/" ,
-  nombre: "Recargas Fullcarga",
-  institucion: "fullcarga",
-},
-{
-  id:21,
-  url: "https://www.minegocioefectivo.com/" ,
-  nombre: "Mi negocio Efectivo",
-  institucion: "Mi negocio Efectivo",
-},
-                        
-{
-  id:22,
-  url: "http://www.eerssa.gob.ec/links-intranet/consultar-facturas-electronicas/" ,
-  nombre: "EERSA Planillas de Luz",
-  institucion: "EERSA",
-},
- {
-  id:24,
-  url: "http://186.178.204.52/kioskos/inicio.php" ,
-  nombre: "EERSA valor pago luz",
-  institucion: "EERSA",
-},                     
- {
-  id:25,
-  url: "https://cuentasbancarias.trabajo.gob.ec/" ,
-  nombre: "Registro de cuenta Bancaria Acta de Finiquito",
-  institucion: "Ministerio de Trabajo",
-},
-{
-  id:26,
-  url: "https://sut.trabajo.gob.ec/mrl/loginContratos.xhtml" ,
-  nombre: "SUT ministerio de trabajo contratos",
-  institucion: "Ministerio de Trabajo",
-},                     
-{
-  id:27,
-  url: "https://sut.trabajo.gob.ec/" ,
-  nombre: "SUT Ministerio de trabajo",
-  institucion: "Ministerio de Trabajo",
-},
-                        {
-  id:28,
-  url: "https://consultasecuador.com/" ,
-  nombre: "Consulta Ecuador",
-  institucion: "Consulta Ecuador",
-},
-]
+  {
+    id: 15,
+    url: "https://apps.registrocivil.gob.ec/portalCiudadano/login.jsf",
+    nombre: "Registro Civil",
+    institucion: "Registro Civil",
+  },
 
+  {
+    id: 16,
+    url: "https://mail.loja.gob.ec:8443",
+    nombre: "Zimbra Municipio Loja",
+    institucion: "Municipio Loja",
+  },
+
+  {
+    id: 17,
+    url: "https://gmail.com",
+    nombre: "Gmail",
+    institucion: "Google",
+  },
+
+  {
+    id: 18,
+    url: "https://outlook.live.com/owa/",
+    nombre: "Hotmail",
+    institucion: "Hotmail",
+  },
+
+  {
+    id: 19,
+    url: "https://www.rplojavirtual.gob.ec/login.xhtml",
+    nombre: "Registro de la propiedad",
+    institucion: "Registro de la propiedad",
+  },
+
+  {
+    id: 20,
+    url: "http://www.c-movil.com.ec/Login.aspx",
+    nombre: "Recargas CMovil",
+    institucion: "CMovil",
+  },
+
+  {
+    id: 21,
+    url: "https://fullcarga-titan.com.ec/",
+    nombre: "Recargas Fullcarga",
+    institucion: "fullcarga",
+  },
+  {
+    id: 21,
+    url: "https://www.minegocioefectivo.com/",
+    nombre: "Mi negocio Efectivo",
+    institucion: "Mi negocio Efectivo",
+  },
+
+  {
+    id: 22,
+    url: "http://186.178.204.61:8093/oficinavirtual/oficina_virtual/",
+    nombre: "EERSA Planillas de Luz",
+    institucion: "EERSA",
+  },
+  {
+    id: 24,
+    url: "http://186.178.204.52/kioskos/inicio.php",
+    nombre: "EERSA valor pago luz",
+    institucion: "EERSA",
+  },
+  {
+    id: 25,
+    url: "https://cuentasbancarias.trabajo.gob.ec/",
+    nombre: "Registro de cuenta Bancaria Acta de Finiquito",
+    institucion: "Ministerio de Trabajo",
+  },
+  {
+    id: 26,
+    url: "https://sut.trabajo.gob.ec/mrl/loginContratos.xhtml",
+    nombre: "SUT ministerio de trabajo contratos",
+    institucion: "Ministerio de Trabajo",
+  },
+  {
+    id: 27,
+    url: "https://sut.trabajo.gob.ec/",
+    nombre: "SUT Ministerio de trabajo",
+    institucion: "Ministerio de Trabajo",
+  },
+  {
+    id: 28,
+    url: "https://consultasecuador.com/",
+    nombre: "Consulta Ecuador",
+    institucion: "Consulta Ecuador",
+  },
+
+  {
+    id: 36,
+    url: "https://docs.google.com/document/d/1bkisEMqkpt2ljfTkWpVSb8ZVkVHDplBV/edit?fbclid=IwZXh0bgNhZW0CMTAAAR0Iljhw3eh1DhoJ4gGoa6vY9XrmRgZzNUf4RLVrN91LIZe90qzgLHx7keI_aem_AdCNSEs9SsrynlD1ZXIIiZHSNfl8VSwq92sqsTw0ZawXkm-Icfr9fQXHQaPcAll5BD39rc2_SIDmUUDSFXaChTQt",
+    nombre: "Fichan de inscripcción 2024-2025 Inmaculada",
+    institucion: "Inmaculada",
+  },
+];
